de2c30fcb1e9ba62c19498854ad69006f8862567
instorecode <alex.goncalves@instore.com.br>
2014-06-20 13:15:50 -0300

    20.06.2014

---
+++ src/main/java/br/com/instore/core/orm/Test2.java
@@ -0,0 +1,9 @@
+package br.com.instore.core.orm;
+
+
+public class Test2 {
+    public static void main(String[] args) {
+        int [] numeros = new int[]{0,1,2,2,52,14,69,14,147,258,6,25,85,96};
+        
+    }
+}
--- src/main/resources/br/com/instore/scripts/dump.sql
+++ src/main/resources/br/com/instore/scripts/dump.sql
@@ -61,8 +61,8 @@ insert into perfil values(2, 'Administrador');
 
 INSERT INTO funcionalidade VALUES (1, '/clientes', 'Cliente' , 'fa-building' , 0 , 1 ); 
 INSERT INTO funcionalidade VALUES (2, '/cliente/cadastrar', 'Formulário de cadastro cliente' , 'fa-building'    , 1 , 1 ); 
-INSERT INTO funcionalidade VALUES (3, '/cliente/atualizar', 'Formulário de atualização cliente' , 'fa-building' , 1 , 0 ); 
-INSERT INTO funcionalidade VALUES (4, '/cliente/remover', 'Formulário de atualização cliente' , 'fa-building'   , 1 , 0 ); 
+INSERT INTO funcionalidade VALUES (3, '/cliente/atualizar/{id}', 'Formulário de atualização cliente' , 'fa-building' , 1 , 0 ); 
+INSERT INTO funcionalidade VALUES (4, '/cliente/remover/{id}', 'Formulário de atualização cliente' , 'fa-building'   , 1 , 0 ); 
 
 insert into perfil_funcionalidade select null, idfuncionalidade , idperfil from perfil , funcionalidade;
 insert into perfil_usuario select null, idperfil , idusuario from perfil , usuario;

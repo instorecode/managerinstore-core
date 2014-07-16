121452aaa0c54dea3e029d637a33980b61f40cb7
instorecode <alex.goncalves@instore.com.br>
2014-07-16 18:03:08 -0300

    16.07.2014

--- src/main/java/br/com/instore/core/orm/SessionFactoryUtils.java
+++ src/main/java/br/com/instore/core/orm/SessionFactoryUtils.java
@@ -94,8 +94,8 @@ public class SessionFactoryUtils {
         properties.put("hibernate.show_sql", true);
         properties.put("javax.persistence.validation.mode", "none");
         properties.put("hibernate.connection.username", "root");
-        properties.put("hibernate.connection.password", "instore@#");
-//        properties.put("hibernate.connection.password", "");
+//        properties.put("hibernate.connection.password", "instore@#");
+        properties.put("hibernate.connection.password", "");;
 
 //        if (Environment.env() == Environment.Env.DEVELOPMENT) {
 //            properties.put("hibernate.connection.username", "root");

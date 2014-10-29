6d2b54e9805af6ece1fc330df10710423d896ca5
instorecode <alex.goncalves@instore.com.br>
2014-10-29 12:39:08 -0200

    29.10.2014 12:38:00

---
+++ src/main/java/br/com/instore/core/orm/XmlAnnotation.java
@@ -0,0 +1,15 @@
+package br.com.instore.core.orm;
+
+
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface XmlAnnotation {
+     public String value();
+}

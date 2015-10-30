f0a43ec587c16f8f0097ebe278f09078fa9e0b21
instorecode <alex.goncalves@instore.com.br>
2015-10-30 19:30:47 -0200

    30/10/2015

--- src/main/java/br/com/instore/dump/DumpGeral.java
+++ src/main/java/br/com/instore/dump/DumpGeral.java
@@ -31,9 +31,9 @@ public class DumpGeral {
     public static void main(String[] args) {
         String urlparadox = "jdbc:paradox:/c:/Users/TI-Caio/Desktop/bancos/San Marino/paradox/";
         String urlexp = "C:\\Users\\TI-Caio\\Desktop\\bancos\\San Marino\\EXP\\musica.exp";
-       
-        lerProgramacaoDoBanco(urlparadox);
-        lerComercialDoBanco(urlparadox);
+//        lerCategoriasDoBanco(urlparadox);
+//        lerProgramacaoDoBanco(urlparadox);
+//        lerComercialDoBanco(urlparadox);
         lerMusicasDoExp(urlexp);
     }
 
@@ -282,7 +282,91 @@ public class DumpGeral {
         }
     }
 
-    
+    public static void lerCategoriasDoBanco(String url) {
+        try {
+            Class.forName("com.hxtt.sql.paradox.ParadoxDriver").newInstance();
+
+            Connection con = DriverManager.getConnection(url, "", "");
+
+            String sql = "select * from categoria";
+
+            Statement stmt = con.createStatement();
+            stmt.setFetchSize(10);
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            ResultSetMetaData resultSetMetaData = rs.getMetaData();
+            int iNumCols = resultSetMetaData.getColumnCount();
+
+            for (int i = 1; i <= iNumCols; i++) {
+//                System.out.println(resultSetMetaData.getColumnLabel(i) + "  " + resultSetMetaData.getColumnTypeName(i));
+            }
+
+            String inserts = "";
+            String inserts2 = "";
+            while (rs.next()) {
+                inserts += "\n";
+                inserts2 += "\n";
+                inserts += "INSERT INTO categoria_geral VALUES([C],1,[T]);";
+                inserts2 += "INSERT INTO audiostore_categoria VALUES([codigo], " + idclienteFinal + ", [categoria],  [data_inicio],  [data_final],  [tipo],  [tempo],  [cod_interno]);";
+
+                for (int i = 1; i <= iNumCols; i++) {
+
+                    if ("Codigo".equals(resultSetMetaData.getColumnLabel(i).trim())) {
+                        String value = "" + (rs.getShort(i) + idclienteFinal);
+                        String cintern = ("" + rs.getShort(i)).replace("'", "\"");
+
+                        if (cintern.length() > 3) {
+                            cintern = cintern.substring(0, 3);
+                        } else {
+                            if (cintern.length() < 3 ) {
+                                cintern = StringUtils.leftPad(cintern, 3, "0");
+                            }
+                        } 
+
+                        inserts = inserts.replace("[C]", "'" + value.replace("'", "\"") + "'");
+                        inserts2 = inserts2.replace("[codigo]", "'" + value.replace("'", "\"") + "'");
+                        inserts2 = inserts2.replace("[cod_interno]", "'" + cintern + "'");
+                    }
+
+                    if ("Categoria".equals(resultSetMetaData.getColumnLabel(i).trim())) {
+                        String value = rs.getString(i);
+                        inserts = inserts.replace("[T]", "'" + value.replace("'", "\"") + "'");
+                        inserts2 = inserts2.replace("[categoria]", "'" + value.replace("'", "\"") + "'");
+                    }
+
+                    if ("DataInicio".equals(resultSetMetaData.getColumnLabel(i).trim())) {
+                        String value = new SimpleDateFormat("yyyy-MM-dd").format(rs.getDate(i));
+                        inserts2 = inserts2.replace("[data_inicio]", "'" + value.replace("'", "\"") + "'");
+                    }
+
+                    if ("DataFinal".equals(resultSetMetaData.getColumnLabel(i).trim())) {
+                        String value = new SimpleDateFormat("yyyy-MM-dd").format(rs.getDate(i));
+                        inserts2 = inserts2.replace("[data_final]", "'" + value.replace("'", "\"") + "'");
+                    }
+
+                    if ("Tipo".equals(resultSetMetaData.getColumnLabel(i).trim())) {
+                        String value = rs.getString(i);
+                        inserts2 = inserts2.replace("[tipo]", "'" + value.replace("'", "\"") + "'");
+                    }
+
+                    if ("Tempo".equals(resultSetMetaData.getColumnLabel(i).trim())) {
+
+                        String value = (null != rs.getDate(i) ? new SimpleDateFormat("HH:mm:ss").format(rs.getDate(i)) : "00:00:00");
+                        inserts2 = inserts2.replace("[tempo]", "'" + value.replace("'", "\"") + "'");
+                    }
+                }
+            }
+            System.out.println(inserts);
+            System.out.println(inserts2);
+            rs.close();
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void lerMusicasDoExp(String url) {
         List<String> nomes = new ArrayList<String>();
         try {
